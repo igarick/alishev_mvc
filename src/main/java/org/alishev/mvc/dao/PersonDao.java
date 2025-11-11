046f7c9f12ab45f102b6a3612ebb7b90e60547cd
Igor Work <keaerta@gmail.com>
2025-11-11 14:52:36 +0500

    mvc_lesson_8-9

---
+++ src/main/java/org/alishev/mvc/dao/PersonDao.java
@@ -0,0 +1,38 @@
+package org.alishev.mvc.dao;
+
+import org.alishev.mvc.model.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "a"));
+        people.add(new Person(++PEOPLE_COUNT, "s"));
+        people.add(new Person(++PEOPLE_COUNT, "d"));
+        people.add(new Person(++PEOPLE_COUNT, "q"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
---
+++ src/main/java/org/alishev/mvc/model/Person.java
@@ -0,0 +1,15 @@
+package org.alishev.mvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Person {
+    private int id;
+    private String name;
+}
